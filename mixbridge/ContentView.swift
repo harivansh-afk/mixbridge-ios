201e4b44f52fe5501f68d72ca823cfc5ea597e20
harivansh-afk <rathiharivansh@gmail.com>
2025-11-09 18:08:49 -0500

    Initial Commit

---
+++ mixbridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mixbridge
+//
+//  Created by Harivansh Rathi on 11/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
