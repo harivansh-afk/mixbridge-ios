820784d830ff887ced3f30519c603c0953bcfd3d
Harivansh Rathi <rathiharivansh@gmail.com>
2025-12-02 21:20:51 -0500

    update account sheet and music player view

--- mixbridge/Components/ExpandedMusicPlayer.swift
+++ mixbridge/Components/ExpandedMusicPlayer.swift
@@ -219,7 +219,7 @@ struct ExpandedPlayerView: View {
         _displayedNext = State(initialValue: nextTrack)
         _displayedPrevious = State(initialValue: previousTrack)
         _showQueueSheet = State(initialValue: true)
-        _queueExpansion = State(initialValue: 200) // Queue partially open by default
+        _queueExpansion = State(initialValue: 0) // Queue visible but no displacement
     }
 
     var body: some View {
